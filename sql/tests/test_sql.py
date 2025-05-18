1dfbf2c3a2fdae4850c51e40e8bedbbec2196bb5
Evan Tahler <evan@arcade.dev>
2025-05-18 14:05:38 -0700

    fixture

--- sql/tests/test_sql.py
+++ sql/tests/test_sql.py
@@ -1,23 +1,29 @@
+import pytest
 from arcade.core.schema import ToolSecretItem
 from arcade.sdk import ToolContext
 
 from arcade_sql.tools.sql import discover_tables, execute_query, get_table_schema
 
-context = ToolContext()
-context.secrets = []
-context.secrets.append(
-    ToolSecretItem(
-        key="DATABASE_CONNECTION_STRING", value="postgresql://evan@localhost:5432/postgres"
+
+@pytest.fixture
+def mock_context():
+    context = ToolContext()
+    context.secrets = []
+    context.secrets.append(
+        ToolSecretItem(
+            key="DATABASE_CONNECTION_STRING", value="postgresql://evan@localhost:5432/postgres"
+        )
     )
-)
+
+    return context
 
 
-def test_discover_tables() -> None:
-    assert discover_tables(context) == ["users", "messages"]
+def test_discover_tables(mock_context) -> None:
+    assert discover_tables(mock_context) == ["users", "messages"]
 
 
-def test_get_table_schema() -> None:
-    assert get_table_schema(context, "public", "users") == [
+def test_get_table_schema(mock_context) -> None:
+    assert get_table_schema(mock_context, "public", "users") == [
         "id: int",
         "name: str",
         "email: str",
@@ -27,7 +33,7 @@ def test_get_table_schema() -> None:
     ]
 
 
-def test_execute_query() -> None:
-    assert execute_query(context, "SELECT id, name, email FROM users WHERE id = 1") == [
+def test_execute_query(mock_context) -> None:
+    assert execute_query(mock_context, "SELECT id, name, email FROM users WHERE id = 1") == [
         "(1, 'Mario', 'mario@example.com')"
     ]
